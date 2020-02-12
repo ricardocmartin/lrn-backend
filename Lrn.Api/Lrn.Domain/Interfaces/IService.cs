13c246b5e3b88fcbe44e3431fb2fafd534c38770
Ricardo Castro Martin <CRmart51@NEXTEL.COM.BR>
2020-02-12 18:34:02 -0300

    Creating first structure

---
+++ Lrn.Api/Lrn.Domain/Interfaces/IService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lrn.Domain.Interfaces
+{
+	public interface IService<T> where T : BaseEntity
+	{
+		T Post<V>(T obj) where V : AbstractValidator<T>;
+
+		T Put<V>(T obj) where V : AbstractValidator<T>;
+
+		void Delete(int id);
+
+		T Get(int id);
+
+		IList<T> Get();
+	}
+}
